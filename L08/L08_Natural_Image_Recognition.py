1d39f3c83fdbe446a67b996ba563922c76e47f13
Eunbyul <eunbyul0805@gmail.com>
2024-05-16 17:29:47 +0900

    24.05.16
    Improves performance and reduces overfitting

--- L08/L08_Natural_Image_Recognition.py
+++ L08/L08_Natural_Image_Recognition.py
@@ -4,7 +4,7 @@ import tensorflow.keras.datasets as ds
 import matplotlib.pyplot as plt
 
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense 
+from tensorflow.keras.layers import Dense, Dropout
 from tensorflow.keras.optimizers import Adam
 
 (x_train, y_train), (x_test, y_test) = ds.cifar10.load_data()
@@ -19,11 +19,13 @@ y_test  = tf.keras.utils.to_categorical(y_test, 10)
 dmlp = Sequential()
 dmlp.add(Dense(units=1024, activation="relu", input_shape=(3072, )))
 dmlp.add(Dense(units=512,  activation="relu"))
+dmlp.add(Dropout(0.25))
+
 dmlp.add(Dense(units=512,  activation="relu"))
 dmlp.add(Dense(units=10,   activation="softmax"))
 
 dmlp.compile(loss="categorical_crossentropy", optimizer=Adam(learning_rate=0.0001), metrics=["accuracy"])
-hist = dmlp.fit(x_train, y_train, batch_size=128, epochs=50, validation_data=(x_test, y_test), verbose=2)
+hist = dmlp.fit(x_train, y_train, batch_size=128, epochs=35, validation_data=(x_test, y_test), verbose=2)
 print("정확률 =", dmlp.evaluate(x_test, y_test, verbose=0)[1]*100)
 
 plt.plot(hist.history["accuracy"])
@@ -43,3 +45,4 @@ plt.ylabel("loss")
 plt.legend(["train", "test"])
 plt.grid()
 plt.show()
+
