2c04f3a399d6a7dd44e77d7a19c36e9bf01b0c88
Eunbyul <eunbyul0805@gmail.com>
2024-05-17 17:58:06 +0900

    Update L09_Recognizing_Natural_Images_With_ResNet50.py

--- L09/L09_Recognizing_Natural_Images_With_ResNet50.py
+++ L09/L09_Recognizing_Natural_Images_With_ResNet50.py
@@ -4,7 +4,7 @@ from tensorflow.keras.applcations.resnet50 import ResNet50, preprocess_input, de
 
 model = ResNet50(weights="imagenet")
 
-img = cv.imread("rabbit.jpg")
+img = cv.imread("img/rabbit.jpg")
 x   = np.reshape(cv.resize(img, (224, 224)), (1, 224, 224, 3))
 x   = preprocess_input(x)
 
